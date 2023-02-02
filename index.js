1e09e1651a99a92a37b9904cf6feea558a887755
pkarisa <faith.pendo@student.moringaschool.com>
2023-02-02 12:50:50 +0300

    moongose added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log ('May we learn');
+//imports of the packages to be used
+const express =require('express');
+const mongoose =require('mongoose')
+
+//create an express app
+const app =express();
+
+app.get('/',function(req,res){
+    res.send('hello world/ may node start')
+    })
+    app.listen(3000);
