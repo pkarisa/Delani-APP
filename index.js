54f0395bc70c0a115727ec6a1b231f6afb34d81b
pkarisa <faith.pendo@student.moringaschool.com>
2023-02-24 11:35:45 +0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-//imports of the packages to be used
+//imports of the packages to be used-first step
 const express =require('express');
 const mongoose =require('mongoose')
 
-//create an express app
+//connect app to mongodb
+const connectionString ='mongodb://localhost:27017';
+//create an express app(third step)
 const app =express();
 
 app.get('/',function(req,res){
     res.send('hello world/ may node start')
     })
     app.listen(3000);
+    console.log('server is running')
